7af615d2b19c3dedb4e5c7fa26cb42e05860eb3f
kanghohyeong <hobro.kang@gmail.com>
2026-02-23 00:25:11 +0900

    extract css

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,7 @@ app.set('views', path.join(__dirname, '..', 'views'))
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('/', router)
 
 app.listen(PORT, () => {
